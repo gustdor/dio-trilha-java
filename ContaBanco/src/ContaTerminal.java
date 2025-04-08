289a88df9f586254b5595de238674d9a4deb191b
coffeeandit-aluno <aluno@coffeeandit.com.br>
2025-04-08 13:12:13 -0300

    inclusao do scanner

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,5 +1,27 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+       
+        System.out.println("Digita o numero: ");
+        int Numero = sc.nextInt();
+
+        System.out.println("Digita a agencia: ");
+        String Agencia = sc.next();
+
+        System.out.println("Digita seu nome: ");
+        String NomeCliente = sc.next();
+
+        System.out.println("Digita o saldo: ");
+        float Saldo = sc.nextFloat();
+
+
+        System.out.println("Olá " + NomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é " + Agencia + 
+        ", conta " + Numero + " e seu saldo " + Saldo + " já esta disponível para saque.");
+
+           
+
     }
 }
